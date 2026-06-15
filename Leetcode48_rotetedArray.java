27a02278044a4becb393f685fcccfafee11a4062
Dibyajyoti Ray Choudhury <141916458+Dibya2005@users.noreply.github.com>
2026-06-16 00:46:01 +0530

    Update Leetcode48_rotetedArray.java

--- Leetcode48_rotetedArray.java
+++ Leetcode48_rotetedArray.java
@@ -1,1 +1,35 @@
 
+class Solution {
+    public void rotate(int[][] matrix) {
+        //step1: transpose of metrix 
+        int n=matrix.length;
+        //step2:reverse rows of metrix1
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                //if we initialized j from 0
+                //the swaping will happen two times beacause we use onply sigle arr
+                //so we consider  
+               int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+       
+
+        for(int i=0;i<n;i++){
+             int l=0;
+            int r=n-1;
+        while(l<r){
+            int temp=matrix[i][l];
+                matrix[i][l]=matrix[i][r];
+                matrix[i][r]=temp;
+                l++;
+                r--;
+        }
+        }
+
+
+
+        
+    }
+}
