f3970c307bea81936f1cbf2982adafe2993f674d
Dibyajyoti Ray Choudhury <141916458+Dibya2005@users.noreply.github.com>
2026-06-15 19:01:38 +0530

    Create Missingelementfromarraywithduplicate.java

---
+++ Missingelementfromarraywithduplicate.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        //marking 
+        // traverse the array when encounter +ve val print the num at the same time
+        for(int i=0;i<nums.length;i++){
+            int num=Math.abs(nums[i]);
+            int pos=nums[i]-1;
+            if (nums[pos] > 0) {
+    nums[pos] = -nums[pos];
+}
+        }
+        //tranverse to find the missing ele
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<0){
+                ans.add(i+1);
+            }
+        }
+        return ans;
+
+    }
+}
