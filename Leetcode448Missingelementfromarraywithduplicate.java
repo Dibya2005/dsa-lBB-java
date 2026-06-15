9418e7f7d99c98f2117764efc51a7410875ba9e8
Dibyajyoti Ray Choudhury <141916458+Dibya2005@users.noreply.github.com>
2026-06-15 19:06:23 +0530

    Update and rename Missingelementfromarraywithduplicate.java to Leetcode448Missingelementfromarraywithduplicate.java

--- Leetcode448Missingelementfromarraywithduplicate.java
+++ Leetcode448Missingelementfromarraywithduplicate.java
@@ -1,22 +1,24 @@
 class Solution {
-    public List<Integer> findMissingElements(int[] nums) {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+    
         List<Integer> ans = new ArrayList<>();
         //marking 
         // traverse the array when encounter +ve val print the num at the same time
         for(int i=0;i<nums.length;i++){
             int num=Math.abs(nums[i]);
-            int pos=nums[i]-1;
+            int pos=num-1;
             if (nums[pos] > 0) {
     nums[pos] = -nums[pos];
 }
         }
         //tranverse to find the missing ele
         for(int i=0;i<nums.length;i++){
-            if(nums[i]<0){
+            if(nums[i]>0){
                 ans.add(i+1);
             }
         }
         return ans;
 
+    
     }
 }
