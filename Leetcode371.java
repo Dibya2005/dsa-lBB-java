49c62d58c5c79866ca5ea1dc5c0a41c5f242f61a
Dibyajyoti Ray Choudhury <141916458+Dibya2005@users.noreply.github.com>
2026-06-19 12:39:01 +0530

    Create Leetcode371.java

---
+++ Leetcode371.java
@@ -0,0 +1,11 @@
+//sum of two integer without using + or -
+class Solution {
+    public int getSum(int a, int b) {
+    while (b != 0) {
+        int carry = (a & b) << 1;
+        a = a ^ b;
+        b = carry;
+    }
+    return a;
+}
+}
